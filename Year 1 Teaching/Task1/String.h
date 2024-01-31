0b8f6317a81d55e0c56daa085689b04f90e24b7e
julian-pahor <julian.pahor1998@gmail.com>
2024-01-31 22:08:45 +1030

    added test solution

---
+++ Year 1 Teaching/Task1/String.h
@@ -0,0 +1,39 @@
+#pragma once
+class String
+{
+public:
+
+	String(const char* string);
+	~String();
+
+	int Length();
+
+	char CharacterAt(int index);
+
+	bool EqualTo(const char* string);
+
+	void Append(const char* string);
+
+	void Prepend(const char* string);
+
+	const char* CStr();
+
+	void ToLower();
+
+	void ToUpper();
+
+	int Find(const char* findString);
+
+	int Find(int startIndex, const char* findString);
+
+	void Replace(const char* findString, const char* replaceString);
+
+	void ReadFromConsole();
+
+	void WriteToConsole();
+
+private:
+
+	char* str; 
+};
+
---
+++ Year 1 Teaching/Task1/Task1.cpp
@@ -0,0 +1,83 @@
+// Task1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "String.h"
+
+//int main()
+//{
+//    int total = 0;
+//    int count = 0;
+//    int number = 1;
+//
+//    while (count < 15) {
+//        total += number;
+//        number += 2;
+//        count += 1;
+//    }
+//
+//    std::cout << "Total of the first 15 odd numbers is " << total << std::endl;
+//
+//    return 0;
+//}
+
+////Triangle Problem
+//int main()
+//{
+//    ////Looping sum solution
+//
+//    //int triangleNumb = 10; //Represents which triangle we are trying to calculate
+//
+//    //int dots = 0;
+//
+//    //for (int i = 0; i < triangleNumb; i++) //for loop iterates over each line in the triangle and adds to the dot count
+//    //{
+//    //    dots += i + 1;
+//    //}
+//
+//    //std::cout << "For the " << triangleNumb << "th Triangle, there are " << dots << " number of dots" << std::endl;
+//
+//
+//    //"Better" solution
+//
+//    int triangleNumb = 10;
+//
+//    int dots = (triangleNumb * (triangleNumb + 1)) * 0.5f; //Formula for calculating numb of dots
+//    //Imagine a second identical triangle to form a rectangle, then simply getting half the area of that rectangle
+//
+//    std::cout << "For the " << triangleNumb << "th Triangle, there are " << dots << " number of dots" << std::endl;
+//
+//}
+
+int main()
+{
+    
+
+    /*int i = 0;
+    bool inputting = true;
+
+    std::cout << "Please write a number between 1-10" << std::endl;
+
+    while (inputting)
+    {
+
+        
+
+        std::cin >> i;
+
+        if (std::cin.fail() || i < 1 || i > 10)
+        {
+            system("cls");
+            std::cin.clear();
+            std::cin.ignore(std::cin.rdbuf()->in_avail());
+            std::cout << "Your input was invalid please try again. Please try again." << std::endl;
+        }
+        else
+        {
+            inputting = false;
+        }
+    }
+
+    std::cout << "Well done you passed the test. Your chosen number was " << i << "." << std::endl;
+    */
+}
